9780b90637a16d8093a227db5acf29ea3c128275
清澈的海水 <111341725+clear-sea@users.noreply.github.com>
2025-07-18 18:58:19 +0800

    Delete js directory

--- js/showBlogs.js
+++
@@ -1,6 +0,0 @@
-function showBlog(title,creationTime,releaseTime,auther,htmlFilePath){
-    document.writeln("<div class=\"panel\">");
-    document.writeln("    <a href=\""+htmlFilePath+"\" target=\"_blank\"><strong>"+title+"</strong></a>");
-    document.writeln("    <p>作于: "+creationTime+"&nbsp&nbsp发布于: "+releaseTime+"&nbsp&nbsp作者："+auther+"</p>");
-    document.writeln("</div>");
-}
